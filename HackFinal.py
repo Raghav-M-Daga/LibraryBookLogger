377e6d79e90079aeeb15a69f33897c3ca030e014
Raghav M Daga <raghavdaga2007@gmail.com>
2024-04-14 16:28:50 -0700

    First Public

---
+++ HackFinal.py
@@ -0,0 +1,72 @@
+import pandas as pd
+
+
+def addBook(ID, bookName):
+    data_raw = pd.read_excel("rented.xlsx")
+    data = data_raw.to_numpy()
+    print(data[0])
+    arr=  []
+    unfilled = False
+    curr_users = []
+    filled_len = len(data[0])
+    for item in data:
+        curr_users.append(item[0])
+    
+    i = curr_users.index(ID)
+    for j, value in enumerate(data[i]):
+        if str(value) == "nan":
+            filled_len = j
+            unfilled = True
+            break
+    
+    if unfilled:
+        for item in data:
+            arr.append(item[-1])
+        data_raw.pop(filled_len)
+    else:
+        arr = [""] * len(data)
+    arr[i] = bookName
+    print(arr)
+    data_raw.insert(filled_len, filled_len, arr)
+    data_raw.to_excel("rented.xlsx", index=False)
+
+    # data = data.insert()
+
+
+def removeBook(ID, bookName):
+    data_raw = pd.read_excel("rented.xlsx")
+    data = data_raw.to_numpy()
+    curr_users = []
+    for item in data:
+        curr_users.append(item[0])
+    i = curr_users.index(ID)
+    for j, item in enumerate(data[i]):
+        if item == bookName:
+            break
+    arr = []
+    for item in data:
+        arr.append(item[j])
+    
+    data_raw.pop(j)
+    
+    arr[i] = ""
+
+    data_raw.insert(j, j, arr)
+    data_raw.to_excel("rented.xlsx", index=False)
+
+def showUser(ID):
+    data_raw = pd.read_excel("rented.xlsx")
+    data = data_raw.to_numpy()
+    curr_users = []
+    for item in data:
+        curr_users.append(item[0])
+    i = curr_users.index(ID)
+    print(data[i])
+    
+
+# addBook(101, "NightFall")
+
+# removeBook(202, "b3")
+
+# showUser(303)
+
